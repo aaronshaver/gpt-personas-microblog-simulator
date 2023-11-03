6b84eda3ea3857e291abc3d45056ee4b018fe09d
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-11-03 11:03:25 -0700

    conservative settings while I test some things

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     worker = Worker()
 
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-    for i in range(20):
+    for i in range(1):
         print(worker.get_message())
         print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
