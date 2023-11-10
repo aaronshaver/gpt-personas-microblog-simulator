0a6807769237ca6b2a17d96518b9515a733599e0
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-11-10 14:52:45 -0800

    openai released a new SDK: perform the needed syntax changes

--- worker/worker.py
+++ worker/worker.py
@@ -1,16 +1,15 @@
-import openai
+from openai import OpenAI
 from io_helper.io_helper import file_to_string, minify_string
 from shared import global_config
 from users.users import Users
 import sqlite3
 import json
-import os
 
 
 class Worker:
     def __init__(self):
+        client = OpenAI(api_key=global_config.OPENAI_API_KEY)
         self.users = Users()
-        openai.api_key = global_config.OPENAI_API_KEY
 
     def get_setting(self, path=None):
         if path is None:
@@ -28,7 +27,8 @@ class Worker:
         return file_to_string(path)
 
     def get_system_content(self):
-        s = f"Follow these rules: |{self.get_rules()}| Follow this setting: |{self.get_setting()}| Follow this overall tone: |{self.get_tone()}| "
+        s = f"Follow these rules: |{self.get_rules()}| Follow this setting: |{
+            self.get_setting()}| Follow this overall tone: |{self.get_tone()}| "
         return minify_string(s)
 
     # TODO: refactor this function; it's doing way too much and is hard to test
@@ -48,7 +48,7 @@ class Worker:
             {"role": "system", "content": self.get_system_content()},
             {"role": "user", "content": json_string_user_prompt}
         ]
-        completion = openai.ChatCompletion.create(
+        completion = self.client.chat.completions.create(
             model=global_config.MODEL,
             messages=messages
         )
@@ -63,7 +63,8 @@ class Worker:
             cursor = conn.cursor()
             cursor.execute(
                 "INSERT INTO messages (user_name, message, reply_to_user, reply_to_message) VALUES (?, ?, ?, ?)",
-                (user_prompt['current_user'], message, user_prompt["reply_to_user"], user_prompt["reply_to_message"])
+                (user_prompt['current_user'], message,
+                 user_prompt["reply_to_user"], user_prompt["reply_to_message"])
             )
             conn.commit()
         except Exception as e:
