6ff2620fc40f1bf8a316781867e610b9787d7b57
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-11-17 15:50:43 -0800

    add /admin endpoint that accepts input via HTML form, writes to stream; needs a few more tweaks but it does work

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from shared import global_config
 import hashlib
@@ -31,3 +31,36 @@ def index():
 
     # render the template and return
     return render_template('messages.html', messages=messages)
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    """
+    allows the user to inject a message into the stream (that the bots can later
+    reply to); in other words, makes the application interactive
+
+    note: this doesn't support the nice display of replying to a user; but
+    that could be easily accomplished by adding an 'id' field in the input
+    form and looking up that user+message for writing to reply_to_user and
+    reply_to_message
+    """
+    if request.method == 'POST':
+        current_user = request.form['username']
+        message = request.form['message']
+
+        try:
+            conn = sqlite3.connect(global_config.DB_NAME)
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO messages (user_name, message, reply_to_user, reply_to_message) VALUES (?, ?, ?, ?)",
+                (current_user, message, '', '')
+            )
+            conn.commit()
+        except Exception as e:
+            print(f"Database insert error: {e}")
+        finally:
+            conn.close()
+
+        return redirect(url_for('admin'))
+
+    return render_template('admin.html')
