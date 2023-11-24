8bacc7185328bf82e6aea61923ec590b0cb506de
Aaron Shaver <aaronshaverpdx@gmail.com>
2023-11-24 13:06:56 -0800

    small comments, clean ups

--- shared/global_config.py
+++ shared/global_config.py
@@ -1,13 +1,16 @@
 from enum import Enum
 import os
 
-# OpenAI model
+
 class Model(Enum):
     GPT_4 = "gpt-4"  # more expensive but more capable
     GPT_35_TURBO = "gpt-3.5-turbo"  # cheaper but makes dumb mistakes
-MODEL = Model.GPT_4.value
 
-# percent chance current user replies to recent messages
+
+# set your preferred OpenAI LLM model
+MODEL = Model.GPT_35_TURBO.value
+
+# the percent chance the current user replies to a recent message
 REPLY_CHANCE = 0.75
 
 # set this however you like, e.g. via `export OPENAI_API_KEY=<key>` in ~/.zshrc
@@ -18,7 +21,8 @@ if os.getenv('DOCKER_ENV'):
 else:
     DB_NAME = 'local_database.db'  # local path for development/testing
 
-MAX_MESSAGES = 100
+MAX_MESSAGES = 100  # "safety valve" so we don't spend tons of OpenAI API money
+
 SECONDS_BETWEEN_MESSAGES = 60
 
 # this is how far back the system will search when looking for a recent message
