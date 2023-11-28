7d63d74e556271c286e7505dcf42fbc304b63d42
Aaron Shaver <1993139+aaronshaver@users.noreply.github.com>
2023-11-27 18:41:59 -0800

    fix small bug: remove older minify function from io_helper
    it didn't actually have an effect, due to Python using the last function defined and our tests protected us, but still ugly and should be removed

--- io_helper/io_helper.py
+++ io_helper/io_helper.py
@@ -8,12 +8,6 @@ def file_to_string(path):
         return ''.join(f.readlines())
 
 
-def minify_string(s):
-    s = s.replace('  ', ' ')
-    s = s.replace('\n\n', '\n')
-    return s
-
-
 # save tokens (and money) by compressing the system and user prompt text;
 # removes string sequences that mostly serve human readers
 def minify_string(s):
